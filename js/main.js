544e1688e242788066549d35f354f0e61039073e
Pablo Gamito <pablo@gamito.me>
2019-05-23 18:20:50 +0100

    Add favicon and content template

--- js/main.js
+++ js/main.js
@@ -58,3 +58,17 @@ var s,
 $(document).ready(function () {
     app.init();
 });
+
+$(document).ready(function(){
+
+    $('.input').focus(function(){
+      $(this).parent().find(".label-txt").addClass('label-active');
+    });
+  
+    $(".input").focusout(function(){
+      if ($(this).val() == '') {
+        $(this).parent().find(".label-txt").removeClass('label-active');
+      };
+    });
+  
+  });
